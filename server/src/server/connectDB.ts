37232948bb2f15838fe10c2e255c01656f4dc70f
Agustin Sosa <agustinsosaa81@gmail.com>
2025-10-22 22:03:18 -0300

    fix: arreglando la conexión a la db

--- server/src/server/connectDB.ts
+++ server/src/server/connectDB.ts
@@ -4,7 +4,10 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
     try {
-        mongoose.connect(process.env.MONGO_URI as string || '')
+        mongoose.connect(process.env.MONGO_URI as string || '', {
+            tls: true,
+            tlsAllowInvalidCertificates: true
+        })
         console.log('Conexion satisfactoria');
     } catch (err) {
         console.log('Error al conectarse a la base de datos')
