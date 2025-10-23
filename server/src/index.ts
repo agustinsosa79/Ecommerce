acbe70d0c107322bd61dbd80b18f1631eb454d33
Agustin Sosa <agustinsosaa81@gmail.com>
2025-10-23 01:47:13 -0300

    fix: corrijo origin para trabajr de manera local

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ dotenv.config()
 
 app.use(express.json())
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
     credentials: true
 }))
 app.use(cookieParser())
